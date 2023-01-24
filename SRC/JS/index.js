4f3d44a3e94d10edfd5aec936649ec517040c8aa
ArthurMarvin <123493976+Marvin1423@users.noreply.github.com>
2023-01-24 14:29:36 -0300

    Add files via upload

---
+++ SRC/JS/index.js
@@ -0,0 +1,19 @@
+    const botaoTrailer = document.querySelector(".botao-trailer");
+    console.log(botaoTrailer);
+    const modal = document.querySelector(".modal");
+    const fecharModal = document.querySelector(".fechar-modal");
+    const video = document.getElementById("video");
+    const linkDoVideo = video.src;
+
+    function altenarModal (){
+        modal.classList.toggle("aberto");
+    }
+
+    botaoTrailer.addEventListener("click", () => {
+        altenarModal ();
+    })
+
+    fecharModal.addEventListener("click", () => {
+        altenarModal ();
+        video.setAttribute ("src", linkDoVideo)
+    })
